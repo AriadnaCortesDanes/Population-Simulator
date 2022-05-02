d7a54a9cdff7363cd33a1be740f4c8538a3f8660
Ariadna Cortés Danés <acd200111@gmail.com>
2022-05-02 17:16:47 +0200

    este commit tampoco

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ const options = document.querySelectorAll(".options");
 let numDays = 0;
 
 options.forEach((option) => {
-    option.addEventListener("click", button)
-});
-
-function button() {
+    option.addEventListener("click", function() {
         const pInput = this.id;
 
-        /* random and arrays
+        /* RANDOM AND ARRAYS USEFULL INFORMATION
         const cOptions = ["recarga", "escut", "dispara", "vidas"];
         let cInput = cOptions[Math.floor(Math.random() * 4)];*/
 
@@ -22,11 +19,12 @@ function button() {
         else if (pInput === "Predator") {
             predator();
         }
-}
+    });
+});
 
 function infiniteFood() {
     var i =0;
-    document.getElementById("img-cell" + i+ "-" + i).src = ".red-bunny.png";
+    document.getElementById("img-cell" + i+ "-" + i).src = "https://cdn.pixabay.com/photo/2017/01/31/17/10/bunny-2025642_960_720.png";
 }
 
 function limitedFood() {
